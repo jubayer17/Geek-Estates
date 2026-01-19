a3f48ced541388f534c853599aa8c67aefc04f7d
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-19 18:14:54 +0600

    Refactor Home component layout and import structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,34 @@
+"use client"
+import Companies from "@/components/Home/Companies";
+import CraftedSpaces from "@/components/Home/CraftedSpaces";
 import FeaturedProperties from "@/components/Home/FeaturedProperties";
 import FindProperties from "@/components/Home/FindProperties";
 import HeroSection from "@/components/Home/HeroSection";
 import Banner from "@/components/Home/HeroSection";
+import HowItWorks from "@/components/Home/HowItWorks";
 import PopularRealEstate from "@/components/Home/PopularRealEstate";
+import TestimonialsSection from "@/components/Home/Reviews";
+
+import TopNotchedProperties from "@/components/Home/TopProperties";
 
 import WhyWithUs from "@/components/Home/WhyWithUs";
 
 
 export default function Home() {
   return (
-    <div>
-        <HeroSection/>
-        <WhyWithUs/>
-        <FeaturedProperties/>
-        <FindProperties/>
-        <PopularRealEstate/>
+    <div className="bg-[#FAFAFA]">
+      <HeroSection />
+
+      <WhyWithUs />
+      {/* <Companies /> */}
+      <CraftedSpaces />
+      <TopNotchedProperties />
+      {/* <FeaturedProperties /> */}
+      <FindProperties />
+      <HowItWorks />
+      <TestimonialsSection />
+      {/* <Testimonials /> */}
+      <PopularRealEstate />
     </div>
   );
 }
