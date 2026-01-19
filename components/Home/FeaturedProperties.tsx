cff12376184c6d658be66c09c39ce203cf419a94
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-19 18:15:47 +0600

    Add padding to FeaturedProperties section for improved layout

--- components/Home/FeaturedProperties.tsx
+++ components/Home/FeaturedProperties.tsx
@@ -4,9 +4,9 @@ import FeaturedTabs from './FeaturedTabs'
 
 export default function FeaturedProperties() {
   return (
-    <section className='lg:mt-50 md:mt-30 mt-10'>
-       <TitleSubtitle title='Featured Properties' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.'/>
-        <FeaturedTabs/>
+    <section className='lg:mt-50 md:mt-30 mt-10 px-4 sm:px-6 lg:px-8'>
+      <TitleSubtitle title='Featured Properties' subtitle='Lorem ipsum dolor sit amet, consectetur adipiscing elit.' />
+      <FeaturedTabs />
     </section>
   )
 }
