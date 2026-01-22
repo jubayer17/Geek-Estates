d42fe0b71903c1da076d85031bdddd793bb1d5e5
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-22 12:37:10 +0600

    refactor: update component imports and clean up unused code
    Remove commented out components and update import paths to use consistent capitalization for 'Home' directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,26 @@
 "use client"
-import Companies from "@/components/home/Companies";
-import CraftedSpaces from "@/components/home/CraftedSpaces";
-import FeaturedProperties from "@/components/home/FeaturedProperties";
-import FindProperties from "@/components/home/FindProperties";
-import HeroSection from "@/components/home/HeroSection";
-import Banner from "@/components/home/HeroSection";
-import HowItWorks from "@/components/home/HowItWorks";
-import PopularRealEstate from "@/components/home/PopularRealEstate";
-import TestimonialsSection from "@/components/home/Reviews";
+import CraftedSpaces from "@/components/Home/CraftedSpaces";
+import FindProperties from "@/components/Home/FindProperties";
+import HeroSection from "@/components/Home/HeroSection";
+import HowItWorks from "@/components/Home/HowItWorks";
+import TestimonialsSection from "@/components/Home/Reviews";
 
-import TopNotchedProperties from "@/components/home/TopProperties";
-import LocationSection from "@/components/home/LocationSection";
+import TopNotchedProperties from "@/components/Home/TopProperties";
+import LocationSection from "@/components/Home/LocationSection";
 
-import WhyWithUs from "@/components/home/WhyWithUs";
-import LetsConnect from "@/components/home/LetsConnect";
-import StatsSection from "@/components/home/StatsSection";
+import WhyWithUs from "@/components/Home/WhyWithUs";
+import StatsSection from "@/components/Home/StatsSection";
 export default function Home() {
   return (
     <div className="bg-[#FAFAFA]">
       <HeroSection />
       <WhyWithUs />
       <StatsSection />
-      {/* <Companies /> */}
       <CraftedSpaces />
       <TopNotchedProperties />
-      {/* <FeaturedProperties /> */}
       <FindProperties />
       <HowItWorks />
       <TestimonialsSection />
-      {/* <Testimonials /> */}
-      {/* <PopularRealEstate /> */}
       <LocationSection />
     </div>
   );
