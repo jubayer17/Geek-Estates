3454a9475b836463c0671b637266bb5575c4371f
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-22 15:45:26 +0600

    feat(contact): add ContactFAQ component to contact page
    Add new FAQ section to improve user support experience

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,14 @@
 import ContactBanner from '@/components/Contact/ContactBanner'
 import ContactForm from '@/components/Contact/ContactForm'
+import ContactFAQ from '@/components/Contact/ContactFAQ'
 import React from 'react'
 
 export default function Contact() {
   return (
     <div>
-        <ContactBanner imageSrc='/9.avif'/>
-        <ContactForm/>
+      <ContactBanner />
+      <ContactForm />
+      <ContactFAQ />
     </div>
   )
 }
