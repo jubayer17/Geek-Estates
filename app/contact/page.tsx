a074194508fb323d7cb9dc2fb49c737c1af2d310
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-26 12:40:10 +0600

    feat(contact): add ContactBanner to contact page
    Include reusable ContactBanner component to enhance the contact page layout and provide consistent visual elements.

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,6 @@
 import ContactForm from '@/components/Contact/ContactForm'
 import ContactFAQ from '@/components/Contact/ContactFAQ'
+import ContactBanner from '@/components/reuseable/ContactBanner'
 import React from 'react'
 
 export default function Contact() {
