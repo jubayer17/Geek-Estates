33117f731457f5fe4730240561c88ee4ae4b390e
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-27 11:06:22 +0600

    chore: rename home component import paths to lowercase
    Update import paths from '@/components/Home/' to '@/components/home/' to maintain consistent naming conventions across the codebase.

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 "use client"
-import CraftedSpaces from "@/components/Home/CraftedSpaces";
-import FindProperties from "@/components/Home/FindProperties";
-import HeroSection from "@/components/Home/HeroSection";
-import HowItWorks from "@/components/Home/HowItWorks";
-import TestimonialsSection from "@/components/Home/Reviews";
+import CraftedSpaces from "@/components/home/CraftedSpaces";
+import FindProperties from "@/components/home/FindProperties";
+import HeroSection from "@/components/home/HeroSection";
+import HowItWorks from "@/components/home/HowItWorks";
+import TestimonialsSection from "@/components/home/Reviews";
 
-import TopNotchedProperties from "@/components/Home/TopProperties";
-import LocationSection from "@/components/Home/LocationSection";
+import TopNotchedProperties from "@/components/home/TopProperties";
+import LocationSection from "@/components/home/LocationSection";
 
-import WhyWithUs from "@/components/Home/WhyWithUs";
-import StatsSection from "@/components/Home/StatsSection";
+import WhyWithUs from "@/components/home/WhyWithUs";
+import StatsSection from "@/components/home/StatsSection";
 export default function Home() {
   return (
     <div className="bg-[#FAFAFA]">
