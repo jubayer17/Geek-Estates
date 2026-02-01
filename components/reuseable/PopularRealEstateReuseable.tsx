f356462f80917803984176e9e8d34ddb24d6a77e
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-02-01 15:36:22 +0600

    Restructure project: separate client and server

--- components/reuseable/PopularRealEstateReuseable.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function PopularRealEstateReuseable({text}:{text:string}) {
-  return (
-     <div className="inline-flex items-center rounded-full border border-gray-400 px-4 py-1.5 text-sm font-medium text-gray-700">
-  {text}
-</div>
-  )
-}
