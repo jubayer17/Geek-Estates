022408aab59670dec3de13833f5c7e644a941571
Istiaque Maubub Isti <istiaquemahbub1@gmail.com>
2026-02-07 13:47:26 +0600

    news and blog section added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,10 @@ import { CareerModule } from './career/career.module';
 import { ProjectsModule } from './projects/projects.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
+import { NewsAndBlogsModule } from './news_and_blogs/newsAndBlogs.module';
 
 @Module({
-  imports: [HomeModule, AdminModule, CareerModule, ProjectsModule,AboutModule],
+  imports: [HomeModule, AdminModule, CareerModule, ProjectsModule,AboutModule,NewsAndBlogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
